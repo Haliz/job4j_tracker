311bde8ea1e54de106ffc991c709a956005bdeb9
Haliz <foril2n1@rambler.ru>
2020-10-04 17:03:57 +0300

    2. Local-Variable Type Inference [#267697]

--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -7,32 +7,32 @@ import static org.junit.Assert.assertThat;
 public class PriorityQueueTest {
     @Test
     public void whenHigherPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
     }
 
     @Test
     public void whenLowPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low1", 5));
         queue.put(new Task("low2", 5));
         queue.put(new Task("low3", 5));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc(), is("low1"));
     }
 
     @Test
     public void whenDownPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low1", 5));
         queue.put(new Task("middle", 3));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("low2", 5));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
     }
 }
